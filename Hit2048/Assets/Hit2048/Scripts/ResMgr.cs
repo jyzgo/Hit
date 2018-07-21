0d2171ee5c682d2dcb91d0815602533c6e20e45e
jyzHome <jyzgo@163.com>
2018-07-22 09:41:30 +1000

    add colors

--- Hit2048/Assets/Hit2048/Scripts/ResMgr.cs
+++ Hit2048/Assets/Hit2048/Scripts/ResMgr.cs
@@ -16,16 +16,38 @@ public class ResMgr : MonoBehaviour {
 
     public GameObject Cell;
 
+    Cell[] cells = new Cell[10];
     private void Start()
     {
-        
+        Test();
     }
 
     
 
     void Test()
     {
+        for (int i = 0; i < 10; i++)
+        {
+            var gb = Instantiate<GameObject>(Cell);
+            gb.transform.position = new Vector3(0, 1 - i * 0.6f, 0);
+            var cell = gb.GetComponent<Cell>();
+            cells[i] = cell;
+        }
+    }
 
+    void TestUpdate()
+    {
+        for(int i =0; i <cells.Length;i++)
+        {
+            var cell = cells[i];
+            cell.SetBgColor(cbg[i]);
+            cell.SetCenterColor(Colors[i]);
+        }
+    }
+
+    private void Update()
+    {
+        TestUpdate();
     }
 
 
