e6703a473b634cac2e86566844d44fb5199268b3
jyzHome <jyzgo@163.com>
2018-08-30 10:11:33 +1000

    add ads

--- Hit2048/Assets/Scripts/Ad/AdMgr.cs
+++ Hit2048/Assets/Scripts/Ad/AdMgr.cs
@@ -7,36 +7,9 @@ using UnityEngine;
 
 public static class AdMgr  {
     #region Admob-------------------------
-//#if (UNITY_IOS)
-//    const string BANNER_ID = "ca-app-pub-8151883983314364/7268920130";
-    
-//    const string INTERSTITIAL_ID = "ca-app-pub-8151883983314364/4175852934";
-////#elif (UNITY_ANDROID)
 
-//    const string MAGIC_NATIVE_BANNER_ID = "ca-app-pub-8151883983314364/2313572938";
-//    const string MAGIC_INTERSTITIAL_ID = "ca-app-pub-8151883983314364/3790306139";
-//    const string MAGIC_BANNER = "ca-app-pub-8151883983314364/7016837337";
-    //#endif
-
-
-    const string MAGIC_IOS_INTERSTITIAL_ID = "ca-app-pub-8151883983314364/4175852934";
-    const string MAGIC_IOS_BANNER_ID = "ca-app-pub-8151883983314364/7268920130";
-
-
-    const string OMG_IOS_INSTERSTITIAL_ID = "ca-app-pub-9169799985632280/8700464051";
-    const string OMG_IOS_BANNER_ID = "ca-app-pub-9169799985632280/4013654050";
-
-
-    //#if (UNITY_IOS)
-    //    const string OMG_NATIVE_BANNER_ID = "ca-app-pub-9169799985632280/7223730851";
-    //    const string OMG_INTERSTITIAL_ID = "ca-app-pub-9169799985632280/8700464051";
-    //#elif (UNITY_ANDROID)
-    //    const string OMG_NATIVE_BANNER_ID = "ca-app-pub-9169799985632280/9572081659";
-    //    const string OMG_INTERSTITIAL_ID = "ca-app-pub-9169799985632280/2048814858";
-    //#endif
-
-
-
+    const string HITBANNER_AND = "ca-app-pub-6770182166257156/8144272380";
+    const string HITINTER_AND = "ca-app-pub-6770182166257156/8711248598";
     public static void ShowAdmobInterstitial()
     {
         _interstitial.Show();
@@ -46,9 +19,7 @@ public static class AdMgr  {
     {
 
 #if UNITY_ANDROID
-        string adUnitId =  //SettingMgr.current._adState.AndroidInterstitialID();
-                           // MAGIC_IOS_INTERSTITIAL_ID;
-       OMG_IOS_INSTERSTITIAL_ID;
+        string adUnitId = HITINTER_AND; 
 #elif UNITY_IOS
         string adUnitId = //SettingMgr.current._adState.IOSInterstitialID(); 
         //MAGIC_IOS_INTERSTITIAL_ID;
@@ -157,16 +128,14 @@ public static class AdMgr  {
 
 
 
-    const string ZL_AND_BANNER = "ca-app-pub-7903543313139585/9520800753";
-    const string ZL_IOS_BANNER = "";
+
 
     static void  InitBanner()
     {
         //#if UNITY_EDITOR
         //        string adUnitId = "unused";
 #if UNITY_ANDROID
-        string adUnitId =// MAGIC_IOS_BANNER_ID;//ZL_AND_BANNER;
-            OMG_IOS_BANNER_ID;
+        string adUnitId = HITBANNER_AND;
 #elif UNITY_IOS
 
         string adUnitId = //MAGIC_IOS_BANNER_ID;//ZL_AND_BANNER;
